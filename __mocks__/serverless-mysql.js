ae730da8ce376f7f9198820e01c96367c7a10ddb
twebber90 <twebber@tractorzoom.com>
2020-05-06 14:31:28 -0500

    test: mock getConfig method

--- __mocks__/serverless-mysql.js
+++ __mocks__/serverless-mysql.js
@@ -1,13 +1,16 @@
 const config = jest.fn();
 const end = jest.fn();
+const getConfig = jest.fn();
 const query = jest.fn();
 
 exports.config = config;
 exports.end = end;
+exports.getConfig = getConfig;
 exports.query = query;
 
 module.exports = () => ({
     config,
     end,
+    getConfig,
     query,
 });
